b209d3f5070650891c9961dfde6018b23bffbf24
Sigurd Vilstrup <32160689+SigurdVilstrup@users.noreply.github.com>
2023-01-15 13:06:24 +0100

    adds generated cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { TicketComponent } from './ticket/ticket.component';
 import { ContactComponent } from './contact/contact.component';
 import { PackinglistComponent } from './packinglist/packinglist.component';
+import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -15,6 +16,7 @@ const routes: Routes = [
   { path: 'ticket', component: TicketComponent },
   { path: 'packinglist', component: PackinglistComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'cart', component: CartComponent },
 ];
 
 @NgModule({
@@ -24,6 +26,7 @@ const routes: Routes = [
     HomeComponent,
     ContactComponent,
     PackinglistComponent,
+    CartComponent,
   ],
   imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
