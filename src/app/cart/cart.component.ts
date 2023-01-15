28b80db22fd7129b2c902a97a13424e104a25d47
Sigurd Vilstrup <32160689+SigurdVilstrup@users.noreply.github.com>
2023-01-15 13:40:00 +0100

    adds basic product logic

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Product {
+  name: string;
+  price: number;
+  link: string;
+}
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
+  cartProducts: Product[] = [
+    { name: 'Partoutbillet', price: 600.0, link: '' },
+    { name: 'Partoutbillet', price: 600.0, link: '' },
+    { name: 'Partoutbillet', price: 600.0, link: '' },
+    { name: 'Partoutbillet', price: 600.0, link: '' },
+    { name: 'Endagsbillet', price: 300.0, link: '' },
+    { name: 'Endagsbillet', price: 300.0, link: '' },
+    { name: 'Endagsbillet', price: 300.0, link: '' },
+    { name: 'Kasse øl', price: 150.0, link: '' },
+    { name: 'Kasse øl', price: 150.0, link: '' },
+    { name: 'Kasse øl', price: 150.0, link: '' },
+  ];
 
-  constructor() { }
+  totalPrice?: number;
+
+  constructor() {}
 
   ngOnInit(): void {
+    let totalSum = 0;
+    this.cartProducts.forEach((product) => {
+      totalSum += product.price;
+    });
+    this.totalPrice = totalSum;
   }
-
 }
